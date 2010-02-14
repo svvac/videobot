9b0a8c833b13d23a6b8c92dcf38b2965ca19a208
Simon 'SwordOfPain' Wachter <swordofpain@gmail.com>
2010-02-14 18:13:20 +0100

    Updates raw port status for stepper motor conrol. It now uses pins RC0-3. (refs issue #23)

--- micro/api/move.h
+++ micro/api/move.h
@@ -21,9 +21,9 @@
 #include "../wrapper/io.h"
 #include "../wrapper/port.h"
 
-#define STEPPER_PHASES_NB 8
+#define STEPPER_PHASES_NB 4
 
-static const int stepperPhases[STEPPER_PHASES_NB] = {0x20,0x28,0x08,0x88,0x80,0x82,0x02,0x22};
+static const int stepperPhases[STEPPER_PHASES_NB] = {0x0A, 0x06, 0x05, 0x09};
 
 static void moveMotorRightForwards(outputs*);
 static void moveMotorRightBackwards(outputs*);
